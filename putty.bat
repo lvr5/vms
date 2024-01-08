ef49a4f810c4da1b41f463f8a51339de9cdda6a7
lvr5 <102605930+lvr5@users.noreply.github.com>
2024-01-07 22:12:58 -0300

    Add files via upload

---
+++ putty.bat
@@ -0,0 +1,10 @@
+@echo off
+
+start putty.exe -ssh andrezm@20.251.115.40 -pw _Lvr1234567890
+start putty.exe -ssh andrezm@20.125.132.152 -pw _Lvr1234567890
+start putty.exe -ssh andrezm@4.225.165.31 -pw _Lvr1234567890
+start putty.exe -ssh andrezm@4.225.165.140 -pw _Lvr1234567890
+start putty.exe -ssh andrezm@20.21.120.22 -pw _Lvr1234567890
+start putty.exe -ssh andrezm@20.215.184.217 -pw _Lvr1234567890
+start putty.exe -ssh andrezm@4.232.157.133 -pw _Lvr1234567890
+start putty.exe -ssh andrezm@20.217.196.21 -pw _Lvr1234567890
